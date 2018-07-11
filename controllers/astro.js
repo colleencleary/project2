f71936af9e4660032ad60cfc3fd98b25ab87c664
colleencleary <cleary.ce@gmail.com>
2018-07-10 20:38:27 -0400

    profile page and edit mostly working

--- controllers/astro.js
+++ controllers/astro.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const bodyParser = require ( 'body-parser' );
 const session = require('express-session');
 const methodOverride = require('method-override');
-
+const Users = require('../models/users.js');
 //___________________
 // Middleware
 //___________________
@@ -38,16 +38,53 @@ router.get('/', (req,res)=>{
     res.render('index.ejs');
   }
 });
-
+// go to forums
 router.get('/forums', (req,res)=>{
+  console.log(req.session.currentUser);
   res.render('forums.ejs', {
     currentUser: req.session.currentUser
   });
 });
 
+
+
+// edit user profile
+router.get('/users/:id/edituser', (req,res)=>{
+  Users.findById(req.params.id, (error, foundUser) => {
+    res.render('edituser.ejs', {
+      currentUser: req.session.currentUser
+    });
+  })
+});
+
+// edit user profile
+router.get('/users/:id/editpersonal', (req,res)=>{
+  Users.findById(req.params.id, (error, foundUser) => {
+    res.render('editpersonal.ejs', {
+      currentUser: req.session.currentUser
+    });
+  })
+});
+
+
+
+router.put('/users/:id', (req, res)=>{
+    Users.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedModel)=>{
+        res.redirect('/astro/users/:id');
+    });
+});
+// view profile
+router.get('/users/:id', (req,res)=>{
+  Users.findById(req.params.id, (error, foundUser) => {
+    res.render('profile.ejs', {
+      currentUser: req.session.currentUser
+    });
+  })
+});
 router.get('/thanks', (req,res)=>{
   res.render('thanks.ejs');
 });
+
 
 
 module.exports = router
